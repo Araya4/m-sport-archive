a0587c53581113d726549ac1ab0316e5fcf8a009
Araya4 <araya.wut@gmail.com>
2024-09-05 09:39:45 +0700

    Update videoList.js

--- videoList.js
+++ videoList.js
@@ -1,14 +1,14 @@
 const videoID = ['Iy8HmOAQQVs','oqTs2sZ-fN0','ymO5tIxUqAg',
     'k1cLYX7vQUeAq2Bohym','1005053180','k21bXdHi0IcyA0Bohys',
     'BveD70tcbvw',
-    'k7KBqEOhI5euvcBohyw','k1smwNPBOlXmmxBoi66','k7eD97Vs5XIdJRBohU4',
+    'B-KAxMjLAoo','k7eD97Vs5XIdJRBohU4',
     'k5ig3amq3N9uKUBnSPG','k7lMVXISoDpxCTBnSPM','k1TYjff43offNMBnSPI','k7eqy67Di1MkGYBnSPK',
     'k3eFbpdOZ0gIzcBnTcU','k7D9LtNtZzpmz0BnTcW','k5sLKLKuSg4sm2Bohyk',
     'k1HvwGmLnMMzWOBohyo','k3vsOdvhndcHyYBohyy',
     'q0vP9USXMS8',
     'k6vTADP42oEgNYBohyu',];
 
-const youtubeSet = new Set([0,1,2,,6,19]);
+const youtubeSet = new Set([0,1,2,,6,7,18]);
 const vimeo = new Set([4]);
 
 var videoUrl = videoID.map((ele,idx)=>{
